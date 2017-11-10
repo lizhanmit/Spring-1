f97e704bb218d278dcafeca5e64540635cb8fbb3
LZ <lizhan_mit@outlook.com>
2017-11-11 09:22:06 +1100

    Config beans.

--- src/main/java/com/zhanlearning/spring/beans/collections/Main.java
+++ src/main/java/com/zhanlearning/spring/beans/collections/Main.java
@@ -1,5 +1,6 @@
 package com.zhanlearning.spring.beans.collections;
 
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,5 +15,16 @@ public class Main {
 		Person person3 = (Person) ctx.getBean("person3");
 		System.out.println(person3);
 		
+		NewPerson newPerson = (NewPerson) ctx.getBean("newPerson");
+		System.out.println(newPerson);
+		
+		DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+		System.out.println(dataSource);
+		
+		Person person4 = (Person) ctx.getBean("person4");
+		System.out.println(person4);
+		
+		Person person5 = (Person) ctx.getBean("person5");
+		System.out.println(person5);
 	}
 }
